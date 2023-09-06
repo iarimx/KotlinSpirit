1a300dbf55e5b1320ff6b7fe1f72bf50141f0d52
iarim <iarimx@gmail.com>
2023-09-06 16:04:55 +0300

    KS-1-5 print number of seconds in space from first lesson task5

--- src/main/kotlin/lesson_1/lesson_1_task_5_new.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5_new.kt
@@ -5,13 +5,6 @@ fun main() {
     timeInSpace = 1
     val minutes = 48
     val secondsByte: Byte = 0
-//    println("0$timeInSpace:$minutes:0$secondsByte")
-//    print("0")
-//    print(timeInSpace)
-//    print(":")
-//    print(minutes)
-//    print(":0")
-//    print(secondsByte)
     val result = String.format("%02d:%02d:%02d", timeInSpace, minutes, secondsByte)
     println(result)
 

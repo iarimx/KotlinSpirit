7f9f975f28dd4e941cfea12404beac0805d804b2
iarim <iarimx@gmail.com>
2023-09-06 19:22:26 +0300

    KS-2-1 print arithmetic mean from second lesson task1

---
+++ src/main/kotlin/lesson_2/lesson_2_task1.kt
@@ -0,0 +1,14 @@
+package lesson_2
+
+fun main() {
+    val pete = 3
+    val kate = 4
+    val nik = 3
+    val mike = 5
+    val sum = (pete + kate + nik + mike) / 4f
+    println(sum)
+    // нашел еще такое короткое оригинальное решение с массивами
+    //    val arrayStudents = arrayOf(3, 4, 3, 5)
+    //    println(arrayStudents.average())
+    
+}

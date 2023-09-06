210220620871987a1ae2a83c29756897cdad1f8b
iarim <iarimx@gmail.com>
2023-09-06 04:51:10 +0300

    KS-1-2 print amount of workers from first lesson

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,12 @@
+package lesson_1
+
+fun main() {
+    val numberOfOrder: Int = 75
+    val thanksForPurchase: String = "Благодарим Вас за приобретение всех наших товаров"
+    println(numberOfOrder)
+    println(thanksForPurchase)
+    var countOfWorkers: Int = 2000
+//    println(countOfWorkers)
+    countOfWorkers = 1999
+    println(countOfWorkers)
+}

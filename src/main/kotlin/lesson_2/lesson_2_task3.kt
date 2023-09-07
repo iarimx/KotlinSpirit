e37932ff0ad2cb67432029853d60118db60287eb
iarim <iarimx@gmail.com>
2023-09-08 00:32:23 +0300

    KS-2-3 print new greating from second lesson task3

--- src/main/kotlin/lesson_2/lesson_2_task3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task3.kt
@@ -9,7 +9,6 @@ fun main() {
     val travelResultHour = checkOutHour + travelTimeHour
     val result = String.format("%02d:%02d", travelResultHour, travelResultMinutes)
     println(result)
-
-
-
+    // Все.... я просто сломал голову над этой элементарной задачей. Я знаю ответ 17:16
+    // но как его вывести я не понимаю. Весь интернет перелопатил... Сдаюся!!!Help
 }

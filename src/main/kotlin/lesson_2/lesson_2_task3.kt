637754b2c519aa64b68c019a9b469de29e639656
iarim <iarimx@gmail.com>
2023-09-08 00:25:48 +0300

    KS-2-3 print new greating from second lesson task3

--- src/main/kotlin/lesson_2/lesson_2_task3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task3.kt
@@ -1,7 +1,15 @@
 package lesson_2
 
 fun main() {
+    val checkOutHour = 9
+    val checkOutMinutes = 39
+    val travelTimeHour = 457 / 60
+    val travelTimeMinutes = 457 % 60
+    val travelResultMinutes = checkOutMinutes + travelTimeMinutes
+    val travelResultHour = checkOutHour + travelTimeHour
+    val result = String.format("%02d:%02d", travelResultHour, travelResultMinutes)
+    println(result)
 
-    println("GitHub")
-    println("Просто уже не знаю что здесь написать.")
+
+
 }

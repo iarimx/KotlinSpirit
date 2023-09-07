bd0523f807a5f118e64bcf90fabadf85f38e46c4
iarim <iarimx@gmail.com>
2023-09-07 21:40:04 +0300

    KS-2-4 print baff from crystal from second lesson task2

---
+++ src/main/kotlin/lesson_2/lesson_2_task4.kt
@@ -0,0 +1,10 @@
+package lesson_2
+
+fun main() {
+    val crystal = 7
+    val ironOre = 11
+    val baffCrystal = 7 + 7 * 20/100 // Ч+20% = Ч + Ч*20/100
+    val baffIronOre = 11 + 11 * 20/100
+    println(baffCrystal)
+    println(baffIronOre)
+}

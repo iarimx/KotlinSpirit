2632c707842050c2c13f6cca8f59757d51963c97
iarim <iarimx@gmail.com>
2023-09-08 13:13:04 +0300

    KS-2-3 print new greating from second lesson task3

--- src/main/kotlin/lesson_2/lesson_2_task3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task3.kt
@@ -1,14 +1,14 @@
 package lesson_2
 
+import kotlin.math.pow
+
 fun main() {
     val checkOutHour = 9
     val checkOutMinutes = 39
-    val travelTimeHour = 457 / 60
-    val travelTimeMinutes = 457 % 60
-    val travelResultMinutes = checkOutMinutes + travelTimeMinutes
-    val travelResultHour = checkOutHour + travelTimeHour
-    val result = String.format("%02d:%02d", travelResultHour, travelResultMinutes)
-    println(result)
-    // Все.... я просто сломал голову над этой элементарной задачей. Я знаю ответ 17:16
-    // но как его вывести я не понимаю. Весь интернет перелопатил... Сдаюся!!!Help
+    val travelTime = 457
+    val arrivalHours = (checkOutHour + (checkOutMinutes + travelTime) / 60) % 24
+    // (9 + (39 + 457) / 60) % 24 Да.... век живи, век учись... Элементарно Ватсон
+    val arrivalMinutes = (checkOutMinutes + travelTime) % 60
+    // 39 + 457 % 60
+    println("$arrivalHours:$arrivalMinutes")
 }

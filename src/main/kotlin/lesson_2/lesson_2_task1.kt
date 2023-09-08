d1fc45d441ebec68291ab8680b417d3ece09477f
iarim <iarimx@gmail.com>
2023-09-08 14:05:13 +0300

    KS-2-3 print arithmetical mean from second lesson task1

---
+++ src/main/kotlin/lesson_2/lesson_2_task1.kt
@@ -0,0 +1,10 @@
+package lesson_2
+
+fun main() {
+    val studentOne = 3
+    val studentTwo = 4
+    val studentThree = 3
+    val studentFour = 5
+    val sum = studentOne + studentTwo + studentThree + studentFour / 4f
+    println(sum)
+}

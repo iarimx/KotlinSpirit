22175d72ddc7f6d6e1c554572dacd75f59c1423d
iarim <iarimx@gmail.com>
2023-09-09 14:11:34 +0300

    KS-2-2 print medium company pay from second lesson task2

--- src/main/kotlin/lesson_2/lesson_2_task2.kt
+++ src/main/kotlin/lesson_2/lesson_2_task2.kt
@@ -2,15 +2,18 @@ package lesson_2
 
 fun main() {
     val permanentWorkers = 50
+    val payOneWorker = 30000
     val traineeWorker = 30
     val payTraineeWorker = 20000
-    val commonPayTraineeWorker = traineeWorker * payTraineeWorker
-    val payOneWorker = 30000
-    val commonPayWorkers = permanentWorkers * payOneWorker // расходы компании на постоянных сотрудников
+    val commonPayWorkers = permanentWorkers * payOneWorker // общая зарплата постоянных работников
+    val commonPayTraineeWorker = traineeWorker * payTraineeWorker // общая зарплата стажеров
+    val commonPay = commonPayTraineeWorker + commonPayWorkers
+    val mediumPay = commonPay / (permanentWorkers + traineeWorker)
+    // 1. расходы компании на постоянных сотрудников
     println(commonPayWorkers)
-    val commonPay = commonPayTraineeWorker + commonPayWorkers // расходы компании после прихода стажеров
+    // 2. расходы компании после прихода стажеров
     println(commonPay)
-    val mediumPay = commonPay / 80 // средняя зарплата сотрудника
+    // 3. средняя зарплата сотрудника
     println(mediumPay)
 
 }

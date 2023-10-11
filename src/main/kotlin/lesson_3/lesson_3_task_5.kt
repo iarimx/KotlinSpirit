7647c401d9a09ac0280e6f23ab046f3933248287
iarim <iarimx@gmail.com>
2023-10-11 21:57:01 +0300

    KS-3-5 print parse chess from third lesson task5

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -1,5 +1,12 @@
 package lesson_3
 
 fun main() {
+    println("D2-D4;0")
+    var whereFrom = "D2"
+    var where = "D4"
+    var chessMove = 0
 
+    println(whereFrom)
+    println(where)
+    println(chessMove)
 }

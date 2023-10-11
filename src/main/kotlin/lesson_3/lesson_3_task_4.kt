2c7038a399767c3bcee0f1e369608e93124da399
iarim <iarimx@gmail.com>
2023-10-11 21:44:53 +0300

    KS-3-4 print chess move from third lesson task4

--- src/main/kotlin/lesson_3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -5,17 +5,18 @@ fun main() {
     var where = "E4"
     var chessMove = 1
 
-    println("$whereFrom на $where; $chessMove")
+    println("[$whereFrom - $where; $chessMove]")
 
     whereFrom = "E4"
     where = "E5"
     chessMove = 2
 
-    println("$whereFrom на $where; $chessMove")
-    // Добрый вечер. Иван я опять не понимаю условия задачи. Первая часть понятна.
-    // А вторая вероятно предполагает какие то доп. знания.
-    // Определить, какое значение будет в переменной "куда" для фигуры, стоящей на D2
-    // и продвинувшейся на одну клетку вперед. Я понимаю что это вероятно пешка и пойдет
-    // она на D3. Но почему она появилась и как ее определить я не понимаю. Спасибо.
+    println("[$whereFrom - $where; $chessMove]")
+
+    chessMove = 3
+    whereFrom = "D2"
+    where = "D4"
+
+    println("[$whereFrom - $where; $chessMove]")
 
 }

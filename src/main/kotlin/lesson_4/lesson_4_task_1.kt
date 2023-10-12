b7d50526abfc862161222ba01b498eb7e057aeb5
iarim <iarimx@gmail.com>
2023-10-12 22:41:37 +0300

    KS-4-1 print free table lesson four task1

--- src/main/kotlin/lesson_4/lesson_4_task_1.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_1.kt
@@ -1,2 +1,14 @@
 package lesson_4
 
+fun main() {
+    var todayTable = 13
+    var tomorrowTable = 4
+
+    val todayResult = todayTable < COMMON_TABLE
+    val tomorrowResult = tomorrowTable < COMMON_TABLE
+
+    println("Доступность столиков на сегодня: $todayResult " + "\nДоступность столиков на завтра: $tomorrowResult")
+
+
+}
+const val COMMON_TABLE = 13
